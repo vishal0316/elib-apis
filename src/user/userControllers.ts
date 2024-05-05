eacc88bb29d1cf57352ab6b34b42008b06ffa5d9
vishal0316 <vr987195@gmail.com>
2024-05-05 19:57:01 +0530

    JWT

--- src/user/userControllers.ts
+++ src/user/userControllers.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -21,17 +23,27 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // password #
   const hashPassword = await bcrypt.hash(password, 10);
 
+  // process
+
   const newUser = await userModel.create({
     name,
     email,
     password: hashPassword,
   });
 
-  //
+  // token generation
+
+  const token = sign(
+    {
+      sub: newUser._id,
+    },
+    config.jwtSecret as string,
+    { expiresIn: "7d" }
+  );
 
   // Response
 
-  res.json({ id: newUser._id });
+  res.json({ accessToken: token });
 };
 
 export { createUser };

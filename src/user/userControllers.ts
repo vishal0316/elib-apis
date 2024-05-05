4c8930999ac7278a3e445a29fe794a51352dd031
vishal0316 <vr987195@gmail.com>
2024-05-05 23:43:16 +0530

    login endpoint

--- src/user/userControllers.ts
+++ src/user/userControllers.ts
@@ -54,10 +54,36 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
     // Response
 
-    res.json({ accessToken: token });
+    res.status(201).json({ accessToken: token });
   } catch (error) {
     return next(createHttpError(500, "Error while signing the jwt token"));
   }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return next(createHttpError(404, "User not found."));
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return next(createHttpError(400, "Username or password incorrect!"));
+  }
+
+  // Create accesstoken
+  const token = sign({ sub: user._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+    algorithm: "HS256",
+  });
+
+  res.json({ accessToken: token });
+};
+
+export { createUser, loginUser };

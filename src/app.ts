fb52ba38397f69fade1af192e56976a19b68cf5d
vishal0316 <vr987195@gmail.com>
2024-05-29 20:24:13 +0530

    cors fix

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
-    // credentials: true,
+    origin: ["http://localhost:5173", "https://elib-dashboard.onrender.com"],
+    credentials: true,
   })
 );
 
